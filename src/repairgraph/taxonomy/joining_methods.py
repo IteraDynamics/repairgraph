f5e42c86a115990e40c28aadcfafb0ede9462edb
IteraDynamics <andrew@moonwire.app>
2026-05-21 14:25:20 -0400

    taxonomy: add joining method vocabulary

---
+++ src/repairgraph/taxonomy/joining_methods.py
@@ -0,0 +1,21 @@
+JOINING_METHODS = {
+    "spot_weld": [
+        "2-plate spot weld",
+        "3-plate spot weld",
+        "4-plate spot weld"
+    ],
+    "mag_weld": [
+        "MAG plug weld",
+        "MAG welding"
+    ],
+    "mig_brazing": [
+        "double hole MIG brazing",
+        "single hole MIG brazing"
+    ],
+    "adhesive": [
+        "adhesive bonding"
+    ],
+    "hemming": [
+        "hemming"
+    ]
+}
